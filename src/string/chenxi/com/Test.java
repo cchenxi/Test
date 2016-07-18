f1b9ed3b5662e8497ac2a62a2225c4c22b763e82
chenxi <chenxi.et@gmail.com>
2016-07-18 22:17:41 +0800

    用字符分割字符串

---
+++ src/string/chenxi/com/Test.java
@@ -0,0 +1,17 @@
+package string.chenxi.com;
+
+import com.alibaba.fastjson.JSON;
+
+public class Test {
+
+	public static void main(String[] args) {
+		/**
+		 * 类 php explode函数
+		 */
+		String message = "我  18";
+		System.out.println(JSON.toJSONString(message.split(" |，|,")));//["我","","18"]
+		//可以处理多个空格的情况
+		System.out.println(JSON.toJSONString(message.split("[ ]+|，|,")));//["我","18"]
+	}
+
+}
