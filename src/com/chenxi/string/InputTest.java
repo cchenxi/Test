a145240dcab212cea83ad369d7d40a4d5cb4d6d0
chenxi <chenxi.et@gmail.com>
2016-08-10 22:53:42 +0800

    String test

---
+++ src/com/chenxi/string/InputTest.java
@@ -0,0 +1,22 @@
+package com.chenxi.string;
+
+import java.util.Scanner;
+
+/**
+ * Scanner
+ * Created by Administrator on 2016/8/10.
+ */
+public class InputTest {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("what is your name?");
+        String name = in.nextLine();
+
+        System.out.println("how old are you?");
+        int age = in.nextInt();
+
+        System.out.println("hello, " + name + ". you are " + age);
+    }
+}
