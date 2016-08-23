32bf6af4dc10d44dc2a2c477ad25f0fc1979e451
chenxi <chenxi.et@gmail.com>
2016-08-23 23:47:05 +0800

    commit with no message

---
+++ src/com/chenxi/array/LotteryDrawing.java
@@ -0,0 +1,41 @@
+package com.chenxi.array;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by Administrator on 2016/8/14.
+ */
+public class LotteryDrawing {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("How many numbers do you need to draw?");
+        int k = in.nextInt();
+
+        System.out.println("What is the highest number you can draw?");
+        int n = in.nextInt();
+
+        int[] numbers = new int[n];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i + 1;
+        }
+
+        int[] result = new int[k];
+        for (int i = 0; i < result.length; i++) {
+            int r = (int)(Math.random() * n);
+
+            result[i] = numbers[r];
+            numbers[r] = numbers[r - 1];
+            n--;
+        }
+
+        Arrays.sort(result);
+        System.out.println("Bet the following combination,it will make you rich!");
+        for (int r : result) {
+            System.out.println(r);
+        }
+    }
+}
--- src/com/chenxi/string/Test.java
+++ src/com/chenxi/string/Test.java
@@ -2,6 +2,8 @@ package com.chenxi.string;
 
 import com.alibaba.fastjson.JSON;
 
+import java.util.Date;
+
 public class Test {
 
 	public static void main(String[] args) {
@@ -12,6 +14,8 @@ public class Test {
 		System.out.println(JSON.toJSONString(message.split(" |，|,")));//["我","","18"]
 		//可以处理多个空格的情况
 		System.out.println(JSON.toJSONString(message.split("[ ]+|，|,")));//["我","18"]
+
+		System.out.printf("%tc", new Date());
 	}
 
 }
