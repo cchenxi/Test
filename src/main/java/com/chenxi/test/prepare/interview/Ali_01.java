d44df77c9f32fe8c07524869f7d84b2a49103412
chenxi <chenxi.et@gmail.com>
2018-11-15 21:00:57 +0800

    ali

---
+++ src/main/java/com/chenxi/test/prepare/interview/Ali_01.java
@@ -0,0 +1,33 @@
+package com.chenxi.test.prepare.interview;
+
+
+import org.springframework.util.StringUtils;
+
+import redis.clients.jedis.Jedis;
+
+//1. 登录接口, 同一个人十分钟内连续登录5次失败,需要等待30分钟才能尝试登录。
+public class Ali_01 {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("localhost",6379);
+
+        //用户userId第一次登录，设置10分钟过期时间
+        String userId = "userId";
+        jedis.set(userId, "0");
+        jedis.expire(userId, 10 * 60);
+
+
+        //模拟5次登录失败
+        for (int i = 0; i < 5; i++) {
+            doLoginFail(jedis, userId);
+        }
+    }
+
+    private static void doLoginFail(Jedis jedis, String key) {
+        jedis.incr(key);
+        //获取登录次数
+        Integer loginTimes = Integer.valueOf(jedis.get(key));
+        if (loginTimes >= 5) {
+            jedis.expire(key, 30 * 60);
+        }
+    }
+}
---
+++ src/main/java/com/chenxi/test/prepare/interview/Ali_02.java
@@ -0,0 +1,60 @@
+package com.chenxi.test.prepare.interview;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+//2. 实现一个生产者消费者模式，要求：多个生产者、一个消费者，在保证逻辑正确的前提下，尽可能提高生产和消费的性能
+public class Ali_02 {
+    public static void main(String[] args) {
+        //队列
+        BlockingQueue<Integer> blockingQueue = new LinkedBlockingQueue<>(10);
+
+        //生产者线程池
+        ExecutorService produceExecutor = new ThreadPoolExecutor(
+                4,
+                16,
+                60,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(50),
+                new ThreadPoolExecutor.CallerRunsPolicy()
+        );
+        //消费者线程池
+        ExecutorService consumeExecutor = Executors.newSingleThreadExecutor();
+
+        //生产者任务，队列入队
+        Runnable producerTask = () -> {
+            try {
+                int value = 0;
+                while (true) {
+                    blockingQueue.put(value);
+                    System.out.println("produced " + value);
+                    value++;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+        //消费者任务 队列出队
+        Runnable consumerTask = () -> {
+            try {
+                while (true) {
+                    System.out.println("consume:" + blockingQueue.take());
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+        produceExecutor.execute(producerTask);
+
+        consumeExecutor.execute(consumerTask);
+
+        produceExecutor.shutdown();
+        consumeExecutor.shutdown();
+    }
+}
